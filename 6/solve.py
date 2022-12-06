3a78f481a4b0b1e8ae342a4a5c08e73fd92da62e
Brad Warren <bmw@eff.org>
2022-12-06 06:56:21 -0800

    minor cleanup

--- 6/solve.py
+++ 6/solve.py
@@ -3,23 +3,20 @@
 def first_marker(problem_input, num_unique):
     for i in range(num_unique, len(problem_input)):
         if len(set(problem_input[i - num_unique:i])) == num_unique:
-            print(i)
-            return
-    assert False
+            return i
+    assert False, 'No marker found'
 
 def part1(problem_input):
-    first_marker(problem_input, 4)
+    return first_marker(problem_input, 4)
 
 def part2(problem_input):
-    first_marker(problem_input, 14)
+    return first_marker(problem_input, 14)
 
 def main():
     with open('input') as f:
         problem_input = f.read()
-    print('Part 1: ', end='')
-    part1(problem_input)
-    print('Part 2: ', end='')
-    part2(problem_input)
+    print('Part 1:', part1(problem_input))
+    print('Part 2:', part2(problem_input))
 
 if __name__ == '__main__':
     main() 
